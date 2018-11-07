273b1e1b44bd96df20eadfbe7c40dfaba2496b5e
brandon13331 <brandon13331@gmail.com>
2018-11-06 17:51:38 -0800

    Added ISBN class to model package

---
+++ src/main/model/ISBN.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ISBN {
+    private Map<Integer, Integer> numbers;
+
+    public ISBN() {
+        numbers = new HashMap<>();
+    }
+
+    public void addNumber(int digit, int number) {
+        numbers.put(digit, number);
+    }
+
+    public Map<Integer, Integer> getNumbers() {
+        return numbers;
+    }
+}
