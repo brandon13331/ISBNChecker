1300207109adf54e37d2af4c9e51bb153639f50d
brandon13331 <brandon13331@gmail.com>
2018-11-12 02:56:07 -0800

    Added option to find the unknown digit of ISBN number

--- src/main/ui/ISBNChecker.java
+++ src/main/ui/ISBNChecker.java
@@ -22,31 +22,44 @@ public class ISBNChecker {
     private ISBNChecker() {
         scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("1. Check ISBN-10 Number");
-            System.out.println("2. Check ISBN-13 Number");
+            System.out.println("1. ISBN-10 Number");
+            System.out.println("2. ISBN-13 Number");
             System.out.println("3. Quit");
             operation = scanner.nextInt();
             if (operation == 1) {
-                try {
-                    setUp(10);
-                } catch (NegativeIntegerException e) {
-                    System.out.println("Invalid input");
+                System.out.println("1. Check if the number is valid");
+                System.out.println("2. Find the unknown digit");
+                System.out.println("3. Back");
+                operation = scanner.nextInt();
+                if (operation == 1) {
+                    try {
+                        setUp(10);
+                    } catch (NegativeIntegerException e) {
+                        System.out.println("Invalid input");
+                        break;
+                    }
+                    int isbn = 10;
+                    if (checkValidity(isbn)) {
+                        System.out.println("Your ISBN-10 number is valid");
+                    } else
+                        System.out.println("Your ISBN-10 number is invalid");
                     break;
+                } else if (operation == 2) {
+                    System.out.println("Enter the unknown digit");
+                    operation = scanner.nextInt();
+                    break;
+                } else {
+                    new ISBNChecker();
                 }
-                if (checkValidity()) {
-                    System.out.println("Your ISBN-10 number is valid");
-                } else
-                    System.out.println("Your ISBN-10 number is invalid");
-                break;
-            }
-            if (operation == 2) {
+            } else if (operation == 2) {
                 try {
                     setUp(13);
                 } catch (NegativeIntegerException e) {
                     System.out.println("Invalid input");
                     break;
                 }
-                if (checkValidity()) {
+                int isbn = 13;
+                if (checkValidity(isbn)) {
                     System.out.println("Your ISBN-13 number is valid");
                 } else
                     System.out.println("Your ISBN-13 number is invalid");
@@ -60,44 +73,56 @@ public class ISBNChecker {
         for (int i = 1; i < digits + 1; i++) {
             if (i == 1) {
                 System.out.println("Enter the number for the " + i + "st digit");
-                addNumberToISBN(i);
+                if (digits == 10) {
+                    addNumberToISBN10(i);
+                } else
+                    addNumberToISBN13(i);
             } else if (i == 2) {
                 System.out.println("Enter the number for the " + i + "nd digit");
-                addNumberToISBN(i);
+                if (digits == 10) {
+                    addNumberToISBN10(i);
+                } else
+                    addNumberToISBN13(i);
             } else if (i == 3) {
                 System.out.println("Enter the number for the " + i + "rd digit");
-                addNumberToISBN(i);
+                if (digits == 10) {
+                    addNumberToISBN10(i);
+                } else
+                    addNumberToISBN13(i);
             } else {
                 System.out.println("Enter the number for the " + i + "th digit");
-                addNumberToISBN(i);
+                if (digits == 10) {
+                    addNumberToISBN10(i);
+                } else
+                    addNumberToISBN13(i);
             }
         }
     }
 
-    private void addNumberToISBN(int i) throws NegativeIntegerException {
-        if (operation == 1) {
-            int number = scanner.nextInt();
-            if (number < 0) {
-                throw new NegativeIntegerException();
-            }
-            isbn10.addNumber(i, number);
-        } else if (operation == 2) {
-            int number = scanner.nextInt();
-            if (number < 0) {
-                throw new NegativeIntegerException();
-            }
-            if (i % 2 == 0) {
-                isbn13.addNumber(3, number);
-            } else {
-                isbn13.addNumber(1, number);
-            }
+    private void addNumberToISBN10(int i) throws NegativeIntegerException {
+        int number = scanner.nextInt();
+        if (number < 0) {
+            throw new NegativeIntegerException();
+        }
+        isbn10.addNumber(i, number);
+    }
+
+    private void addNumberToISBN13(int i) throws NegativeIntegerException {
+        int number = scanner.nextInt();
+        if (number < 0) {
+            throw new NegativeIntegerException();
+        }
+        if (i % 2 == 0) {
+            isbn13.addNumber(3, number);
+        } else {
+            isbn13.addNumber(1, number);
         }
     }
 
-    private boolean checkValidity() {
+    private boolean checkValidity(int isbn) {
         int number;
         List<Integer> numbers = new ArrayList<>();
-        if (operation == 1) {
+        if (isbn == 10) {
             for (int i = 1; i < 11; i++) {
                 number = i * isbn10.getNumbers().get(i).get(0);
                 numbers.add(number);
@@ -107,11 +132,12 @@ public class ISBNChecker {
                 sum = sum + i;
             }
             // mod 11
-            if (sum % 11 == 0) {
+            int remainder = sum % 11;
+            if (remainder == 0) {
                 return true;
             } else
                 return false;
-        } else if (operation == 2) {
+        } else if (isbn == 13) {
             for (int i = 1; i < 14; i++) {
                 if (i % 2 == 0) {
                     number = 3 * isbn13.getNumbers().get(3).get(i - 1);
